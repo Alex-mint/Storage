b9dd8d937e56541d6208962461156caf0fc91bf1
Alex <soyvitaly@gmail.com>
2022-04-08 20:44:38 +0200

    Add template

--- storage/main/views.py
+++ storage/main/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 
-# Create your views here.
+from .models import Item
+
+
+class Home(ListView):
+    model = Item
+    template_name = 'main/index.html'
+    context_object_name = 'products'
+
